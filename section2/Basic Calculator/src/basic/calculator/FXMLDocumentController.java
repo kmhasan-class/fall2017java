e376b2ea539a824f264a3ad38bea713a28db3e83
Monirul Hasan <kmhasan@gmail.com>
2017-10-18 11:27:56 +0600

    lab task for section 2

--- section2/Basic Calculator/src/basic/calculator/FXMLDocumentController.java
+++ section2/Basic Calculator/src/basic/calculator/FXMLDocumentController.java
@@ -22,16 +22,24 @@ MVC design pattern / framework
 M - Model
 V - View
 C - Controller
-*/
+ */
 public class FXMLDocumentController implements Initializable {
 
     @FXML
     private TextField displayField;
-    
+    /*
+    Lab task
+    Modify the data types to allow for floating point
+    calculations. Just change int to double!
+    */
+    int operand1;
+    int operand2;
+    String operator;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    }
 
     @FXML
     private void handleOneAction(ActionEvent event) {
@@ -56,5 +64,98 @@ public class FXMLDocumentController implements Initializable {
         String newText = oldText + "3";
         displayField.setText(newText);
     }
-    
+
+    @FXML
+    private void handleFourAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        // string concatenation
+        String newText = oldText + "4";
+        displayField.setText(newText);
+    }
+
+    @FXML
+    private void handleAdditionAction(ActionEvent event) {
+        operand1 = Integer.parseInt(displayField.getText());
+        operator = "ADD";
+        displayField.setText("");
+    }
+
+    @FXML
+    private void handleSubtractAction(ActionEvent event) {
+        /*
+        Lab Task
+        When you're reading numbers, you should read double
+        Hint: Double.parseDouble
+        */
+        operand1 = Integer.parseInt(displayField.getText());
+        operator = "SUBTRACT";
+        displayField.setText("");
+    }
+
+    @FXML
+    private void handleEqualAction(ActionEvent event) {
+        operand2 = Integer.parseInt(displayField.getText());
+        int result = 0;
+        switch (operator) {
+            case "ADD":
+                result = operand1 + operand2;
+                break;
+            case "SUBTRACT":
+                result = operand1 - operand2;
+                break;
+                /*
+                Lab Task
+                Add support for MULTIPLY and DIVIDE
+                Also for unary operations like SQRT and SQR
+                Hint: for SQRT use Math.sqrt
+                for SQR just multiply the number by itself
+                */
+        }
+        displayField.setText("" + result);
+    }
+
+    @FXML
+    private void handleDotAction(ActionEvent event) {
+        String oldText = displayField.getText();
+        if (!oldText.contains(".")) {
+            String newText = oldText + ".";
+            displayField.setText(newText);
+        }
+    }
+
+    @FXML
+    private void handleZeroAction(ActionEvent event) {
+        /*
+        Lab Task
+        Modify the code to allow a leading zero
+        only when it is followed by a dot
+        Hint: use the startsWith method (look at the Java API)
+        https://docs.oracle.com/javase/7/docs/api/java/lang/String.html
+        */
+        String oldText = displayField.getText();
+        // string concatenation
+        String newText = oldText + "0";
+        displayField.setText(newText);        
+    }
+
+    @FXML
+    private void handleClearAction(ActionEvent event) {
+        /*
+        Lab Task
+        Clear the rightmost digit
+        Hint: use the substring method from the String class
+        (again, look at the String API)
+        */
+    }
+
+    @FXML
+    private void handleAllClearAction(ActionEvent event) {
+        displayField.setText("");
+        //displayField.clear();
+    }
+
+    /*
+    Lab Task
+    Write code for all the remaining buttons
+    */
 }
