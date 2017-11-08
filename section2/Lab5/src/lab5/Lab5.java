0024a557b4668707aabbfb358cf95ef1d5873a1e
Monirul Hasan <kmhasan@gmail.com>
2017-11-08 12:58:39 +0600

    Lab 5 for section 2

--- section2/Lab5/src/lab5/Lab5.java
+++ section2/Lab5/src/lab5/Lab5.java
@@ -18,7 +18,11 @@ public class Lab5 {
         // ADD YOUR CODE HERE
         // method is supposed to compute the arithmetic mean
         // of all the numbers in the list
-        return 0.0;
+        double sum = 0;
+        for (int i = 0; i < integerList.size(); i++)
+            sum = sum + integerList.get(i);
+        double avg = sum / integerList.size();
+        return avg;
     }
 
     public double standardDeviation(ArrayList<Integer> integerList) {
@@ -44,13 +48,29 @@ public class Lab5 {
         // check if all the characters are digits
         // String s = "124ab";
         // s.charAt(0) >= '0' && s.charAt(0) <= '9'
-        return 0;
+        int count = 0;
+        for (int i = 0; i < stringList.size(); i++) {
+            String s = stringList.get(i);
+            boolean isInteger = true;
+            for (int j = 0; j < s.length() && isInteger; j++) {
+//                if (s.charAt(j) < '0' || s.charAt(j) > '9')
+//                    isInteger = false;
+                
+                if (!Character.isDigit(s.charAt(j)))
+                    isInteger = false;
+            }
+            if (isInteger)
+                count++;
+        }
+        return count;
     }
     
     public ArrayList<String> reverseList(ArrayList<String> stringList) {
         // ADD YOUR CODE HERE
         // method is supposed to create a new list
         // with all the elements kept in reverse order
+        // Hint: ["A", "B", "D", "C"]
+        // ["C", "D", "B", "A"]
         return null;
     }
     
@@ -59,11 +79,20 @@ public class Lab5 {
         ArrayList<String> stringList = new ArrayList<>();
         stringList.add("Apple");
         stringList.add("Banana");
-        stringList.add("125");
+        stringList.add("125.01");
         stringList.add("Cranberry");
         stringList.add("33");
+        stringList.add("33a55");
         System.out.println(stringList);
         System.out.println(reverseList(stringList));
+        System.out.println("# of integers: " + countIntegers(stringList));
+        
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(30);
+        intList.add(32);
+        intList.add(31);
+        intList.add(36);
+        System.out.println("Average: " + average(intList));
     }
     
     /**
