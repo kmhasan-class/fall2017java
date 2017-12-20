1f66d153c3336e088730db5f69752579a4aeb159
Monirul Hasan <kmhasan@gmail.com>
2017-12-20 12:39:14 +0600

    paint lab

--- section2/Lab8/src/lab8/FXMLDocumentController.java
+++ section2/Lab8/src/lab8/FXMLDocumentController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package paint.application;
+package lab8;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -14,6 +14,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 /**
  *
@@ -24,7 +25,7 @@ public class FXMLDocumentController implements Initializable {
     @FXML
     private Canvas drawingCanvas;
     private GraphicsContext gc;
-    
+
     private void drawGrid(double dx, double dy, Color color) {
         gc.setStroke(color);
         for (int i = 0; i <= drawingCanvas.getWidth() / dx; i++)
@@ -33,26 +34,28 @@ public class FXMLDocumentController implements Initializable {
             gc.strokeLine(0, i * dy, drawingCanvas.getWidth(), i * dy);
     }
     
-    private void drawBangladeshFlag() {
-        Color green = Color.rgb(0, 103, 71);
-        gc.setFill(green);
-        gc.fillRect(0, 0, 500, 300);
+    private void drawStickFigure1() {
+        gc.setLineWidth(5);
+        gc.setStroke(Color.BLACK);
+        // head
+        gc.strokeOval(40, 40, 120, 120);
+        // left eye
+        gc.strokeOval(60, 80, 20, 20);
+        // right eye
+        gc.strokeOval(120, 80, 20, 20);
         
-        Color red = Color.rgb(218, 41, 28);
-        gc.setFill(red);
-        gc.fillOval(225 - 100, 150 - 100, 200, 200);
+        gc.strokeLine(100, 160, 100, 260);
+        gc.strokeLine(100, 260, 60, 320);
+        
+        gc.strokeArc(60, 60, 80, 80, 200, 140, ArcType.OPEN);
     }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gc = drawingCanvas.getGraphicsContext2D();
-        //gc.strokeRect(10, 10, 300, 200);
-        //gc.strokeOval(10, 10, 300, 300);
-        //gc.strokeLine(0, 0, drawingCanvas.getWidth(), drawingCanvas.getHeight());
         drawGrid(20, 20, Color.LIGHTGRAY);
         drawGrid(100, 100, Color.GRAY);
-        //drawBangladeshFlag();
+        drawStickFigure1();
     }    
-
-
+    
 }
