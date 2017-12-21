e3966140541cd3be99219369eaa03f449cbb58af
Monirul Hasan <kmhasan@gmail.com>
2017-12-21 12:46:36 +0600

    drawing

--- section3/Paint Application/src/paint/application/FXMLDocumentController.java
+++ section3/Paint Application/src/paint/application/FXMLDocumentController.java
@@ -20,39 +20,35 @@ import javafx.scene.paint.Color;
  * @author kmhasan
  */
 public class FXMLDocumentController implements Initializable {
-
+    
     @FXML
     private Canvas drawingCanvas;
     private GraphicsContext gc;
     
-    private void drawGrid(double dx, double dy, Color color) {
-        gc.setStroke(color);
-        for (int i = 0; i <= drawingCanvas.getWidth() / dx; i++)
-            gc.strokeLine(i * dx, 0, i * dx, drawingCanvas.getHeight());
-        for (int i = 0; i <= drawingCanvas.getHeight() / dy; i++)
-            gc.strokeLine(0, i * dy, drawingCanvas.getWidth(), i * dy);
-    }
-    
-    private void drawBangladeshFlag() {
-        Color green = Color.rgb(0, 103, 71);
-        gc.setFill(green);
-        gc.fillRect(0, 0, 500, 300);
-        
-        Color red = Color.rgb(218, 41, 28);
-        gc.setFill(red);
-        gc.fillOval(225 - 100, 150 - 100, 200, 200);
-    }
-    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         gc = drawingCanvas.getGraphicsContext2D();
-        //gc.strokeRect(10, 10, 300, 200);
-        //gc.strokeOval(10, 10, 300, 300);
-        //gc.strokeLine(0, 0, drawingCanvas.getWidth(), drawingCanvas.getHeight());
+//        gc.setStroke(Color.RED);
+//        gc.strokeRect(0, 0, 250, 300);
+//        gc.setFill(Color.rgb(178, 28, 234));
+//        gc.fillRect(10, 10, 200, 150);
+//        gc.strokeLine(0, 0, 125, 300);
         drawGrid(20, 20, Color.LIGHTGRAY);
         drawGrid(100, 100, Color.GRAY);
-        drawBangladeshFlag();
+        
+        gc.setFill(Color.RED);
+        gc.strokeRect(200, 100, 160, 300);
+        gc.fillOval(200, 100, 160, 300);
+        
+        gc.fillOval(400, 200, 100, 100);
     }    
 
-
+    private void drawGrid(int dx, int dy, Color color) {
+        gc.setStroke(color);
+        for (int i = 0; i <= drawingCanvas.getHeight() / dy; i++)
+            gc.strokeLine(0, i * dy, drawingCanvas.getWidth(), i * dy);
+        for (int i = 0; i <= drawingCanvas.getWidth() / dx; i++)
+            gc.strokeLine(i * dx, 0, i * dx, drawingCanvas.getHeight());
+    }
+    
 }
